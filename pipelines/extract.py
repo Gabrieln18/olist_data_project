7e972d446ddb126d6556a10949645d652d952eae
Gabriel Nunes <gabrielnunecosta@gmail.com>
2025-03-31 18:43:11 -0300

    Update extract script

--- pipelines/extract.py
+++ pipelines/extract.py
@@ -18,28 +18,35 @@ def extract_func_pipeline(source_path_csv: str, bronze_delta_path: str, name_tab
         logger.info(f"\033[32m[OK]\033[0m Processo de extração do {source_path_csv} foi concluído.")
 
     except Exception as e:
-        logger.error(f"Erro ao processar {source_path_csv}: {str(e)}")
+        logger.error(f"\033[031m[ERROR]\033[0m Erro ao processar {source_path_csv}: {str(e)}")
 
     finally:
         conn.close()  # isso evita o problema de memory leaks ao duckdb
 
+def extract_func_sqlite(db_path: str, sqlite_table: str, bronze_delta_path: str, name_table: str, mode: str = "overwrite"):
+    conn = duckdb.connect()
+    
+    try:
+        logger.info(f"Iniciando extração da tabela SQLite - {sqlite_table}")
+        # conectar com sqlite usando duckdb
+        conn.execute(f"ATTACH '{db_path}' AS sqlite_db;")
+        dataframe = conn.sql(f"SELECT * FROM sqlite_db.{sqlite_table}").arrow()  # converte para PyArrow Table
+        table_path = os.path.join(bronze_delta_path, name_table)
+        os.makedirs(table_path, exist_ok=True)
+        write_deltalake(table_path, dataframe, mode=mode)
+        logger.info(f"\033[32m[OK]\033[0m Extração da tabela {sqlite_table} concluída e salva em {table_path}.")
+
+    except Exception as e:
+        logger.error(f"\033[031m[ERROR]\033[0m Erro ao processar {sqlite_table}: {str(e)}")
+
+    finally:
+        conn.close()
+
 if __name__ == "__main__":
-    path_dir = Path("../data")
-    files_list_path = [str(file) for file in path_dir.glob("*.csv")]
 
-    # função para normalizar os nomes dos arquivos
-    def extract_name(path: str) -> str:
-        name = os.path.basename(path)  # pega só o nome do arquivo
-        name = name.replace("olist_", "").replace("_dataset", "").replace(".csv", "")  # removendo os prefixos e sufixos
-        return name
+    # extração de dados .csv
+    extract_func_pipeline(source_path_csv="../data/olist_customers_dataset.csv", bronze_delta_path="../delta_lake/bronze", name_table="customers_bronze")
 
-    # cria uma dict {nome_limpo: caminho_original}
-    files_dict = {extract_name(path): path for path in files_list_path}
-
-    bronze_delta_path = "../delta_lake/bronze/"
-    os.makedirs(bronze_delta_path, exist_ok=True)  # Cria se não existir
-
-    # itera sobre os arquivos
-    for dataset, file_path in files_dict.items():
-        bronze_delta_path = f"../delta_lake/bronze/{dataset}"
-        extract_func_pipeline(file_path, bronze_delta_path, name_table=dataset)
+    # extração de dados sqlite, tabelas de leads
+    extract_func_sqlite(db_path="../data/olist.sqlite", sqlite_table="leads_qualified", name_table="leads_qualified_bronze", bronze_delta_path="../delta_lake/bronze")
+    extract_func_sqlite(db_path="../data/olist.sqlite", sqlite_table="leads_closed", name_table="leads_closed_bronze", bronze_delta_path="../delta_lake/bronze")
