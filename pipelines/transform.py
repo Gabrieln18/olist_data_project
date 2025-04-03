760093d18fc65984c6d3e19a9c39cf6295f1478c
Gabrieln18 <gabrielnunecosta@gmail.com>
2025-04-03 20:41:54 -0300

    Add pipelines transform to silver layer: leads_qualified and leads_closed

--- pipelines/transform.py
+++ pipelines/transform.py
@@ -225,3 +225,38 @@ if __name__ == "__main__":
             UPPER(TRIM(seller_state)) AS seller_state
         FROM delta_scan('../delta_lake/bronze/sellers_bronze')
     """, table_name="sellers")
+
+    # leads qualified table
+    transform_pipeline_sql(
+        query="""
+        SELECT
+            mql_id,
+            landing_page_id,
+            CAST(first_contact_date AS TIMESTAMP) AS first_contact_date,
+            LOWER(TRIM(COALESCE(origin, 'N/A'))) AS came_from
+        FROM delta_scan('../delta_lake/bronze/leads_qualified_bronze');
+    """, table_name="leads_qualified_silver"
+    )
+
+    # leads_closed table
+    transform_pipeline_sql(
+        query="""
+        SELECT 
+            mql_id,
+            seller_id,
+            sdr_id,
+            sr_id,
+            CAST(won_date AS TIMESTAMP) AS won_date,
+            LOWER(TRIM(business_segment)) AS business_segment,
+            LOWER(TRIM(lead_type)) AS lead_type,
+            LOWER(TRIM(lead_behaviour_profile)) AS lead_behaviour_profile,
+            COALESCE(has_company, 0) AS has_company,
+            COALESCE(has_gtin, 0) AS has_gtin,
+            COALESCE(NULLIF(average_stock, ''), 'N/A') AS average_stock,
+            LOWER(TRIM(business_type)) AS business_type,
+            COALESCE(declared_product_catalog_size, 0.0) AS declared_product_catalog_size,
+            COALESCE(declared_monthly_revenue, 0.0) AS declared_monthly_revenue
+        FROM delta_scan('../delta_lake/bronze/leads_closed_bronze')
+        """,
+        table_name="leads_closed_silver"
+    )
